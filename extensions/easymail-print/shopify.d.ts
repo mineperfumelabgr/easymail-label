ef6c09ba1e2564b7972ffff421e988b7f0602117
mineperfumelab.gr <info@mineperfumelab.gr>
2026-01-12 15:38:50 +0200

    Fix lint: add description to ts-expect-error in shopify.d.ts

--- extensions/easymail-print/shopify.d.ts
+++ extensions/easymail-print/shopify.d.ts
@@ -1,7 +1,5 @@
 import '@shopify/ui-extensions';
-
-// @ts-expect-error shopify types provided at runtime
-
+// @ts-expect-error Shopify admin globals are injected at runtime
 declare module './src/ActionExtension.jsx' {
   const shopify: import('@shopify/ui-extensions/admin.order-details.action.render').Api;
   const globalThis: { shopify: typeof shopify };
