09abe9fd20f9ff84727f0a65dedd792030140fbf
mineperfumelab.gr <info@mineperfumelab.gr>
2026-01-12 17:41:54 +0200

    Fix lint: replace ts-ignore with ts-expect-error in shopify.d.ts

--- extensions/easymail-print/shopify.d.ts
+++ extensions/easymail-print/shopify.d.ts
@@ -1,13 +1,8 @@
 import '@shopify/ui-extensions';
 
 // @ts-expect-error Shopify injects the admin order action API at runtime
+
 declare module './src/ActionExtension.jsx' {
-  const shopify: import(
-    '@shopify/ui-extensions/admin.order-details.action.render'
-  ).Api;
-
-  const globalThis: {
-    shopify: typeof shopify;
-  };
+  const shopify: import('@shopify/ui-extensions/admin.order-details.action.render').Api;
+  const globalThis: { shopify: typeof shopify };
 }
-
